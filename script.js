9d92d441b7f6ecabf364b1bfeced443e3d18427c
Faizal Muhammad <ragil.rayan@gmail.com>
2026-06-26 15:27:40 +0000

    add fitur

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-Script:script.js
+function showPage(p) {
+document.querySelectorAll('.page-section').forEach(s => s.classList.remove('active'));
+document.getElementById(p).classList.add('active');
+window.scrollTo(0,0);
+}
